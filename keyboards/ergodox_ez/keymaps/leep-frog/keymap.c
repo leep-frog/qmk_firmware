be429f0e2e6e7e31e33467bb87f617f7ab8fb80a
Gregory L <gleeper49@gmail.com>
2020-06-09 19:21:54 -0400

    leep-frog add tab commands and swap backspace and delete

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -88,8 +88,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox_pretty(
   // left hand
   VRSN,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,   _______, KC_UP,   KC_7,    KC_UP,   KC_9,     KC_ASTR, KC_F12,
-  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                                    KC_DOWN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PLUS, _______,
+  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,   _______, KC_UP,   KC_TABB, KC_UP,   KC_TABF, KC_ASTR, KC_F12,
+  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                               KC_DOWN, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PLUS, _______,
   _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______,   _______, KC_AMPR, KC_1,    KC_2,    KC_3,     KC_BSLS, _______,
   EEP_RST, _______, _______, _______, _______,       _______, KC_DOT,  KC_0,    KC_EQL,  _______,
                                                RGB_MOD, _______,     RGB_TOG, RGB_SLD,
