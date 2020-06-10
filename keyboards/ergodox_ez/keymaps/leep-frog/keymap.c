f9449e0c554f09f1cc9b67cd12160516988155d9
Gregory L <gleeper49@gmail.com>
2020-06-10 12:05:59 -0400

    Switch space and control

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -58,15 +58,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_SPCB,  KC_SPCF,  KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINUS,
+  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_SPCB,  KC_ESC,   KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINUS,
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB), TG(MDIA), KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLASH,
   KC_LSPO,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,        KC_L,        LT(SYMB, KC_SCLN), KC_RSPC,
   KC_LBRACKET,     KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_SPCB,  KC_SPCF,  KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLSH, KC_RBRACKET,
-  KC_GRAVE,        KC_QUOTE,    LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                                     KC_UP,   KC_DOWN, KC_LBRACKET, KC_RBRACKET, TG(SYMB),
+  KC_LGUI,         KC_QUOTE,    LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                       KC_UP,   KC_DOWN, KC_LBRACKET, KC_RBRACKET, KC_RGUI,
 
-                                                  LALT_T(KC_APPLICATION), KC_HOME,                  KC_PGUP, CTL_T(KC_ESC),
+                                                                 KC_SPCF, KC_HOME,                  KC_PGUP, KC_SPCB,
                                                                            KC_END,                  KC_PGDN,
-                                                     KC_ENTER, KC_LALT, KC_DELETE,                  KC_BSPACE, KC_RCTRL, KC_SPACE
+                                                     KC_LALT, KC_ENTER, KC_DELETE,                  KC_BSPACE, KC_SPACE, KC_RCTRL
 ),
 /* Keymap 1: Symbol Layer
  *
