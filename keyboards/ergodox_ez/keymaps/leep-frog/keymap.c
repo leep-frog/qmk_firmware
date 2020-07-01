2efc6988d3079a860086155588fffd3d7dbcfdd8
Gregory L <gleeper49@gmail.com>
2020-07-01 11:23:52 -0400

    number pad

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -58,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_SPCB,  KC_ESC,   KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINUS,
-  KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB), TG(MDIA), KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLASH,
-  KC_LSPO,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,        KC_L,        LT(SYMB, KC_SCLN), KC_RSPC,
-  KC_LBRACKET,     KC_Z,        KC_X,          KC_C,    KC_V,    KC_B,    KC_SPCB,  KC_SPCF,  KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLSH, KC_RBRACKET,
-  KC_LGUI,         KC_QUOTE,    LALT(KC_LSFT), KC_LEFT, MO(SYMB),                                      KC_UP,   KC_DOWN, KC_LBRACKET, KC_RBRACKET, KC_RGUI,
+  KC_EQL,          KC_1,           KC_2,          KC_3,    KC_4,    KC_5,    KC_SPCB,  KC_ESC,   KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINUS,
+  KC_TAB,          KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB), TG(MDIA), KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLASH,
+  KC_LSPO,         LT(MDIA, KC_A), KC_S,          KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,        KC_L,        LT(SYMB, KC_SCLN), KC_RSPC,
+  KC_LBRACKET,     KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,    KC_SPCB,  KC_SPCF,  KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLSH, KC_RBRACKET,
+  KC_LGUI,         KC_QUOTE,       LALT(KC_LSFT), KC_LEFT, MO(SYMB),                                      KC_UP,   KC_DOWN, KC_LBRACKET, KC_RBRACKET, KC_RGUI,
 
                                                                  KC_SPCF, KC_HOME,                  KC_PGUP, KC_SPCB,
                                                                            KC_END,                  KC_PGDN,
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox_pretty(
   // left hand
   VRSN,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, KC_EXLM, KC_AT,   KC_EXLM, KC_HASH, KC_AT,   _______,   _______, KC_WH_U, KC_TABB, KC_UP,   KC_TABF, KC_ASTR, KC_F12,
+  _______, KC_QUES, KC_AT,   KC_EXLM, KC_HASH, KC_AT,   _______,   _______, KC_WH_U, KC_TABB, KC_UP,   KC_TABF, KC_ASTR, KC_F12,
   _______, KC_AMPR, KC_ASTR, KC_DLR,  KC_RPRN, KC_GRV,                      KC_WH_D, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PLUS, _______,
   _______, KC_PERC, KC_CIRC, KC_PERC, KC_CIRC, KC_TILD, _______,   _______, KC_AMPR, KC_1,    KC_2,    KC_3,     KC_BSLS, _______,
   EEP_RST, _______, _______, _______, _______,       _______, KC_DOT,  KC_0,    KC_EQL,  _______,
@@ -124,9 +124,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MDIA] = LAYOUT_ergodox_pretty(
   // left hand
   _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_SPCB, KC_BTN3, KC_SPCF, _______, _______,
-  _______, _______, _______, KC_WH_U, _______, _______, _______,     _______, KC_WH_U, KC_TABB, KC_MS_U, KC_TABF, MY_PSTE, _______,
-  _______, _______, KC_WH_L, KC_WH_D, KC_WH_R, _______,                       KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_MPLY,
-  _______, _______, _______, _______, _______, _______, _______,     _______, MY_COPY, KC_WWWB, _______, KC_WWWF, _______, _______,
+  _______, _______, KC_7,    KC_8,    KC_9,    _______, _______,     _______, KC_WH_U, KC_TABB, KC_MS_U, KC_TABF, MY_PSTE, _______,
+  _______, _______, KC_4,    KC_5,    KC_6,    _______,                       KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_MPLY,
+  _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,     _______, MY_COPY, KC_WWWB, _______, KC_WWWF, _______, _______,
   _______, _______, _______, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
 
                                                _______, _______,     _______, _______,
