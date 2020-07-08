6f1f4f66333fd5af959cfc319a70319eeb3a6a08
Gregory L <gleeper49@gmail.com>
2020-07-08 09:07:35 -0400

    curly braces

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EQL,          KC_1,           KC_2,          KC_3,    KC_4,    KC_5,    KC_SPCB,  KC_ESC,   KC_6,    KC_7,    KC_8,        KC_9,        KC_0,    KC_MINUS,
   KC_TAB,          KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB), TG(MDIA), KC_Y,    KC_U,    KC_I,        KC_O,        KC_P,    KC_BSLASH,
   KC_LSPO,         LT(MDIA, KC_A), KC_S,          KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,        KC_L,        LT(MDIA, KC_SCLN), KC_RSPC,
-  LT(SYMB, KC_LBRACKET),     KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,    KC_SPCB,  KC_SPCF,  KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLSH, LT(SYMB, KC_RBRACKET),
+  KC_LCBR,         KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,    KC_SPCB,  KC_SPCF,  KC_N,    KC_M,    KC_COMMA,    KC_DOT,      KC_SLSH, KC_RCBR,
   KC_LGUI,         KC_QUOTE,       LALT(KC_LSFT), KC_LEFT, MO(SYMB),                                      KC_UP,   KC_DOWN, KC_LBRACKET, KC_RBRACKET, KC_RGUI,
 
                                                                  KC_SPCF, KC_HOME,                  KC_PGUP, KC_SPCB,
