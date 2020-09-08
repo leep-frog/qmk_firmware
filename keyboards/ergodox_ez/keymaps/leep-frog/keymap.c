9f8b8f904994c39052a64469aebc537cef6245e2
Gregory L <gleeper49@gmail.com>
2020-09-07 22:18:48 -0400

    percent symbol

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox_pretty(
   // left hand
   VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-  _______, _______, KC_COLN, KC_EQL,  KC_CIRC, KC_BSLS, _______,   _______, KC_PIPE, KC_7,    KC_8,    KC_9,     KC_0,    KC_F12,
+  _______, _______, KC_COLN, KC_EQL,  KC_CIRC, KC_BSLS, _______,   _______, KC_PIPE, KC_7,    KC_8,    KC_9,     KC_PERC, KC_F12,
   _______, KC_AMPR, KC_ASTR, KC_DLR,  KC_HASH, KC_LCBR,                     KC_RCBR, KC_4,    KC_5,    KC_6,     KC_0,    _______,
   _______, KC_TILD, KC_EXLM, KC_AT,   KC_GRV,  KC_LBRC, _______,   _______, KC_RBRC, KC_1,    KC_2,    KC_3,     KC_0,    _______,
   EEP_RST, _______, _______, _______, _______,       _______, KC_DOT,  KC_0,    KC_EQL,  _______,
