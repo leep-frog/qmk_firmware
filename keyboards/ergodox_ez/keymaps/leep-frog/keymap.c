a6e086d7dd1c34c4c1526d3d31d57853125141b8
leep-frog <gleeper49@gmail.com>
2021-02-07 16:34:00 -0500

    minor edits

--- keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
+++ keyboards/ergodox_ez/keymaps/leep-frog/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                                  KC_SPCF, KC_HOME,                  KC_PGUP, KC_SPCB,
                                                                            KC_END,                  KC_PGDN,
-                                   LALT_T(KC_LCBR), LT(SYMB, KC_ENTER), LALT_T(KC_DELETE),                  RCTL_T(KC_BSPACE), RSFT(KC_SPACE), RGUI_T(KC_RCBR)
+                                   LALT_T(KC_LCBR), LSFT_T(KC_ENTER), LALT_T(KC_DELETE),                  RCTL_T(KC_BSPACE), LT(SYMB, KC_SPACE), RGUI_T(KC_RCBR)
 ),
 
 /* Keymap 0: Basic layer
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox_pretty(
   VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
   _______, _______, KC_COLN, KC_EQL,  KC_CIRC, KC_BSLS, _______,   _______, KC_PIPE, KC_7,    KC_8,    KC_9,     KC_PERC, KC_F12,
-  _______, KC_AMPR, KC_ASTR, KC_DLR,  KC_PIPE, KC_LBRC,                     KC_RBRC, KC_4,    KC_5,    KC_6,     KC_0,    KC_PERC,
+  _______, KC_AMPR, KC_ASTR, KC_DLR,  KC_SLSH, KC_LBRC,                     KC_RBRC, KC_4,    KC_5,    KC_6,     KC_0,    KC_PERC,
   _______, KC_TILD, KC_EXLM, KC_AT,   KC_EQL,  KC_PERC, _______,   _______, KC_RBRC, KC_1,    KC_2,    KC_3,     KC_0,    _______,
   EEP_RST, _______, _______, _______, _______,       _______, KC_DOT,  KC_0,    KC_EQL,  _______,
                                                RGB_MOD, _______,     RGB_TOG, RGB_SLD,
