74d164ae73671f708a7d4dd81020abbec1c0388a
leep-frog <gleeper49@gmail.com>
2023-08-27 11:44:28 -0400

    remove comments

--- keyboards/groogerpad/matrix.c
+++ keyboards/groogerpad/matrix.c
@@ -72,8 +72,6 @@ typedef struct __attribute__((packed)) {
 } nina_gamepad_t;
 
 typedef struct {
-  // 0-indexed bit position starting at the least-significant bit
-  // uint8_t gamepad_button_bit;
   uint8_t kb_matrix_row;
   // 0-indexed bit position starting at the least-significant bit
   uint8_t kb_matrix_row_bit;
@@ -95,54 +93,32 @@ button_mapping_t button_mappings[] = {
 
 const uint8_t NUM_BUTTONS = sizeof(button_mappings) / sizeof(button_mappings[0]);
 
-bool blop = false;
-int m_count = 0;
-
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-  /*if (m_count == 1) {
-    SEND_STRING("0");
-    m_count++;
-  } else if (m_count == 50) {
-    SEND_STRING("5");
-    m_count++;
-  } else if (m_count <= 50) {
-    m_count++;
-  }*/
   uart_write(1);
-  // if (uart_available()) {
-    // SEND_STRING("H");
-    // xprintf("Loading controller");
-    nina_gamepad_t gamepad;
-    uart_receive((uint8_t *)(&gamepad), sizeof(nina_gamepad_t));
-    uint16_t button_mask = gamepad.buttons;
 
-    if (button_mask % 2 == 1) {
-      led_on();
-    } else {
-      led_off();
+  nina_gamepad_t gamepad;
+  uart_receive((uint8_t *)(&gamepad), sizeof(nina_gamepad_t));
+  uint16_t button_mask = gamepad.buttons;
+
+  if (button_mask % 2 == 1) {
+    led_on();
+  } else {
+    led_off();
+  }
+
+  bool changed = false;
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    button_mapping_t bm = button_mappings[i];
+    // If the key is marked as pressed
+    bool gamepad_pressed = !!(button_mask & 1);
+    bool key_pressed = !!(current_matrix[bm.kb_matrix_row] & (bm.kb_matrix_row_bit));
+
+    // Toggle the bit if they don't match.
+    if (key_pressed != gamepad_pressed) {
+      current_matrix[bm.kb_matrix_row] ^= (bm.kb_matrix_row_bit);
+      changed = true;
     }
-
-    bool changed = false;
-    if (blop) {
-      // send_byte(button_mappings[0].gamepad_button_bit);
-      send_byte((uint8_t)button_mask);
-    }
-    for (int i = 0; i < NUM_BUTTONS; i++) {
-      button_mapping_t bm = button_mappings[i];
-      // If the key is marked as pressed
-      // bool gamepad_pressed = !!(((uint8_t)button_mask) & (bm.gamepad_button_bit));
-      bool gamepad_pressed = !!(button_mask & 1);
-      bool key_pressed = !!(current_matrix[bm.kb_matrix_row] & (bm.kb_matrix_row_bit));
-
-      // Toggle the bit if they don't match.
-      if (key_pressed != gamepad_pressed) {
-        current_matrix[bm.kb_matrix_row] ^= (bm.kb_matrix_row_bit);
-        changed = true;
-      }
-      button_mask >>= 1;
-    }
-    return changed;
-  // }
-
-  return false;
+    button_mask >>= 1;
+  }
+  return changed;
 }
