11599c48862d4ae31f80f41a8c0be8cf89d13488
leep-frog <gleeper49@gmail.com>
2023-09-19 19:02:40 -0400

    get default to compile

--- keyboards/groogerpad/keymaps/default/keymap.c
+++ keyboards/groogerpad/keymaps/default/keymap.c
@@ -3,6 +3,23 @@
 
 #include QMK_KEYBOARD_H
 
+#include "keymap_introspection.h"
+
+const uint16_t PROGMEM test_combo1[] = {KC_A, KC_B, COMBO_END};
+combo_t key_combos[] = {
+  COMBO(test_combo1, KC_C),
+};
+enum custom_keycodes {
+  BLOOP,
+};
+bool Bloop(keyrecord_t *record) { return false; }
+custom_keycode_handler_t custom_keycode_handlers[] = {
+  [BLOOP] = NULL,
+};
+uint16_t Alt_keycodes[] = {
+  KC_Z,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Roughly the shape of an xbox controller
     [0] = LAYOUT_xbox(
@@ -10,8 +27,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 C(G(KC_LEFT)),                                        C(G(KC_RIGHT)),
                                            _______,                   KC_Y,
                 C(KC_C),          KC_BTN2,          KC_BTN2, KC_X,             KC_B,
-                CK_UP,                     _______,                   KC_A,
-       KC_LEFT,          CK_RGHT,                   C(V),
-                CK_DOWN
+                KC_UP,                     _______,                   KC_A,
+       KC_LEFT,          KC_RGHT,                   C(KC_V),
+                KC_DOWN
     )
 };
