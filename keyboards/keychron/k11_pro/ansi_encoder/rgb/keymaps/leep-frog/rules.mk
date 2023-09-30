b68718111cef2853b388e66885c3040822a66634
leep-frog <gleeper49@gmail.com>
2023-09-30 18:27:36 -0400

    fix matrix driver

--- keyboards/keychron/k11_pro/ansi_encoder/rgb/keymaps/leep-frog/rules.mk
+++ keyboards/keychron/k11_pro/ansi_encoder/rgb/keymaps/leep-frog/rules.mk
@@ -4,7 +4,9 @@ TAP_DANCE_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 KEY_LOCK_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
-#RGB_MATRIX_DRIVER = IS31FL3731
+# RGB_MATRIX_DRIVER needs to be the proper case (had to manually change to lowercase
+# to override the keyboard's info.json file
+RGB_MATRIX_DRIVER = ckled2001
 # AUDIO_ENABLE = yes
 #VPATH += keyboards/gboards/
 #CONSOLE_ENABLE = yes
