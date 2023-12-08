518488920c45dea348d4da56c47b97824b5aa502
leep-frog <gleeper49@gmail.com>
2023-12-08 13:44:17 -0500

    update cr desc

--- users/leep-frog/keyboard-main/leep_cr_desc_kb.c
+++ users/leep-frog/keyboard-main/leep_cr_desc_kb.c
@@ -1,16 +1,16 @@
 
-// TODO: Use exclusively left arrows instead of down to be consistent with editor width.
-
 const uint8_t cr_desc_line_moves[] = {
   // Deployability section
-  1, 1, 1, 3,
+  91, 40, 40, 56,
   // Testing section
-  1, 3, 2, 1, 1, 3,
+  51, 209, 123, 99, 79, 83,
   // Testing section
-  1, 1, 1, 3,
-  // End (move a few lines below the last comment so we see the bottom
+  69, 81, 97, 78,
+  // End (move a one line below the last comment so we see the bottom
   // of the details input box).
-  3,
+  // Note: 53 is number of characters to next line, so (presumably) this number
+  // brings us three lines below the last one
+  53 + 2,
 };
 
 uint8_t cr_desc_line_move_idx = 0;
@@ -49,6 +49,7 @@ bool CrDescProcessHandler(uint16_t keycode, keyrecord_t* record) {
   }
 
   if (cr_desc_line_move_idx >= (sizeof(cr_desc_line_moves)/sizeof(cr_desc_line_moves[0]))) {
+    EndCrDesc(); // In case it wasn't ended before
     return false;
   }
 
@@ -75,12 +76,15 @@ bool CrDescProcessHandler(uint16_t keycode, keyrecord_t* record) {
   }
 
   for (int i = 0; i < cr_desc_line_moves[cr_desc_line_move_idx]; i++) {
-    tap_code16(KC_DOWN);
+    tap_code16(KC_RIGHT);
   }
-  tap_code16(KC_RIGHT);
-  tap_code16(KC_RIGHT);
 
   cr_desc_line_move_idx++;
+
+  // Check if done
+  if (cr_desc_line_move_idx >= (sizeof(cr_desc_line_moves)/sizeof(cr_desc_line_moves[0]))) {
+    EndCrDesc();
+  }
 
   return true;
 }
