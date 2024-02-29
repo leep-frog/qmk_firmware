6f72e7c336708aa1fb4dc4de676939f32618cfb3
leep-frog <gleeper49@gmail.com>
2024-02-29 03:03:43 -0500

    add semi-colon to ella layer

--- keyboards/moonlander/keymaps/leep-frog/keymap.c
+++ keyboards/moonlander/keymaps/leep-frog/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LR_ELLA] = ML_LAYOUT(
         KC_NO,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO,       KC_NO, KC_6, KC_7, KC_8,    KC_9,   KC_0,    _______,
         KC_NO,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO,       KC_NO, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
-        KC_NO,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO,       KC_NO, KC_H, KC_J, KC_K,    KC_L,   KC_NO,   KC_NO,
+        KC_NO,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO,       KC_NO, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_NO,
         KC_NO,  KC_Z, KC_X, KC_C, KC_V, KC_B,                     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCBR,
 
            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
