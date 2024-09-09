0c7c982869ed178b62ac1667bb84e216368147ff
leep-frog <gleeper49@gmail.com>
2024-09-08 23:44:45 -0400

    groogerpedal is working!!!!!

--- keyboards/groogerpedal/keymaps/default/keymap.c
+++ keyboards/groogerpedal/keymaps/default/keymap.c
@@ -8,3 +8,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F
     )
 };
+
+// uint16_t leep_countt = 0;
+
+// bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+//   // if (leep_countt < 100) {
+//   SEND_STRING("S");
+//   // leep_countt++;
+//   wait_ms(5000);
+//   // }
+//   return true;
+// }
--- keyboards/groogerpedal/matrix.c
+++ keyboards/groogerpedal/matrix.c
@@ -9,30 +9,55 @@
 // Max is 1023
 const int blink_time = 125;
 
-void led_on(void) {
-  writePinHigh(D4);
-}
+// void led_on(void) {
+//   writePinHigh(??);
+// }
 
-void led_off(void) {
-  writePinLow(D4);
-}
+// void led_off(void) {
+//   writePinLow(??);
+// }
 
-void led_blink(int times) {
-  for (int i = 0; i < times; i++) {
-    led_on();
-    wait_ms(blink_time);
-    led_off();
-    wait_ms(blink_time);
-  }
-}
+// void led_blink(int times) {
+//   for (int i = 0; i < times; i++) {
+//     led_on();
+//     wait_ms(blink_time);
+//     led_off();
+//     wait_ms(blink_time);
+//   }
+// }
 
 void matrix_init_custom(void) {
   xprintf("Initializing groog controller");
   xprintf("Initing");
-  // setPinInput(D4);
+  setPinInput(D4);
 }
+
+uint8_t lpcount = 0;
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
   bool changed = false;
+  bool pressed = readPin(D4);
+  if ((!!(current_matrix[0] & 1)) != (pressed)) {
+    current_matrix[0] ^= 1;
+    changed = true;
+  }
+  if (lpcount <= 3) {
+    SEND_STRING("A");
+    lpcount++;
+    wait_ms(250);
+  }
+
   return changed;
 }
+
+uint8_t llpcount = 0;
+
+// bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
+//   if (llpcount <= 3) {
+//     SEND_STRING("B");
+//     llpcount++;
+//     wait_ms(250);
+//   }
+
+//   return false;
+// }
