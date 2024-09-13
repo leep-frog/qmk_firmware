b26a3f52be1102a5a739d105d030e2b593be8244
leep-frog <gleeper49@gmail.com>
2024-09-13 16:54:08 -0400

    all pedals activated!!

--- keyboards/groogerpedal/matrix.c
+++ keyboards/groogerpedal/matrix.c
@@ -1,13 +1,17 @@
 #include QMK_KEYBOARD_H
 #include "quantum.h"
 #include "matrix.h"
+#include "analog.h"
 
 uint8_t pedal_pins[] = {
-  D4,
-  D1,
+  F0,
+  F1,
+  F4,
+  F5,
 };
 
 uint8_t num_pedals = 0;
+const uint16_t analog_press_threshold = 125;
 
 void matrix_init_custom(void) {
   num_pedals = sizeof(pedal_pins) / sizeof(uint8_t);
@@ -24,7 +28,7 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
   uint16_t matrix_row_bit = 1;
   for (uint8_t i = 0; i < num_pedals; i++) {
 
-    bool pressed = readPin(pedal_pins[i]);
+    bool pressed = analogReadPin(pedal_pins[i]) < analog_press_threshold;
     if ((!!(current_matrix[0] & matrix_row_bit)) != (pressed)) {
       // Take the XOR of the bit (which always flip that bit)
       current_matrix[0] ^= matrix_row_bit;
--- keyboards/groogerpedal/rules.mk
+++ keyboards/groogerpedal/rules.mk
@@ -2,6 +2,7 @@
 CUSTOM_MATRIX = lite
 SEND_STRING_ENABLE = yes
 SRC += matrix.c
+SRC += analog.c
 
 # From: https://docs.qmk.fm/#/squeezing_avr
 LTO_ENABLE = yes
