cca7c33e1ce83ab86da7817f87a62db0bdc6c30f
leep-frog <gleeper49@gmail.com>
2024-11-12 22:16:09 -0500

    fix enter

--- users/leep-frog/keyboard-main/leep_to_ctrl_kb.c
+++ users/leep-frog/keyboard-main/leep_to_ctrl_kb.c
@@ -1,6 +1,10 @@
+#include "timer.h"
+
 // To ctrl layer functions
 static uint16_t ctrl_timer;
 static bool     ctrl_interrupted = false;
+static bool     ctrl_press_mode = false;
+static uint32_t ctrl_last_unpress = 0;
 
 void ToCtrl_handled(uint16_t keycode) {
     if (keycode != ToCtrlKeycode) {
@@ -8,16 +12,35 @@ void ToCtrl_handled(uint16_t keycode) {
     }
 }
 
+// TODO: Eliminate chance of accidental enter by setting value in matrix scan
+// use similar concept to ToCtrl_handled, but ToCtrl_scan and put in scan code in main.c
+// TODO: Add test? Eh maybe not
+
 bool ToCtrl_run(keyrecord_t *record, custom_keycode_value_t *_) {
     if (record->event.pressed) {
+      if (timer_elapsed32(ctrl_last_unpress) < TAPPING_TERM) {
+        ctrl_press_mode = true;
+        register_code16(KC_ENTER);
+      } else {
         ctrl_interrupted = false;
         ctrl_timer       = timer_read();
         layer_on(CtrlLayer);
+      }
+      return false;
+    }
+
+
+    // Unpress
+    ctrl_last_unpress = timer_read32();
+    if (ctrl_press_mode) {
+      ctrl_press_mode = false;
+      unregister_code16(KC_ENTER);
     } else {
-        layer_off(CtrlLayer);
-        if (!ctrl_interrupted && timer_elapsed(ctrl_timer) < TAPPING_TERM) {
-            tap_code16(KC_ENTER);
-        }
+      layer_off(CtrlLayer);
+      if (!ctrl_interrupted && timer_elapsed(ctrl_timer) < TAPPING_TERM) {
+        ctrl_last_unpress = timer_read32();
+        tap_code16(KC_ENTER);
+      }
     }
     return false;
 }
