a066945e8330f1a558527a7bda64f617b934e2ab
leep-frog <gleeper49@gmail.com>
2026-02-15 21:13:07 -0500

    fix ctrl t in mouse

--- keyboards/ploopyco/mouse/keymaps/leep-frog/leep_tap_dance.c
+++ keyboards/ploopyco/mouse/keymaps/leep-frog/leep_tap_dance.c
@@ -78,7 +78,7 @@ tap_dance_action_t tap_dance_actions[] = {
     // Alt dance
     [TDK_ALT] = LEEP_TD_CLICK_KC_HOLD_LAYER_ON_FIRST_CLICK_ONLY(KC_BTN3, LR_ALT),
     // Ctrl dance
-    [TDK_CTRL] = LEEP_TD_CLICK_KC_HOLD_LAYER(KC_BTN3, C(KC_T)),
+    [TDK_CTRL] = LEEP_TD_CLICK_KC_HOLD_LAYER(C(KC_T), LR_CTRL),
     // Ctrl+tab or next page in browser
     [TDK_CTRL_TAB] = LEEP_TD_CLICK_FN_HOLD_KC(td_ctrl_tab, LEEP_TD_NOVAL(), A(KC_RIGHT)),
     // Ctrl+shift+tab of previous page in browser
