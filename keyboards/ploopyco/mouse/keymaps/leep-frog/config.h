60635dcd4404a6f7f9b74921fbe55d758cf16a22
leep-frog <gleeper49@gmail.com>
2026-07-28 13:23:35 -0400

    fix ploopyco mouse

--- keyboards/ploopyco/mouse/keymaps/leep-frog/config.h
+++ keyboards/ploopyco/mouse/keymaps/leep-frog/config.h
@@ -4,6 +4,17 @@
 
 #include "../../config.h"
 
+#define MS_BTN1 KC_BTN1
+#define MS_BTN2 KC_BTN2
+#define MS_BTN3 KC_BTN3
+#define MS_WHLU KC_WH_U
+#define MS_WHLD KC_WH_D
+#define MS_WHLL KC_WH_L
+#define MS_WHLR KC_WH_R
+#define MS_ACL0 KC_ACL0
+#define MS_ACL1 KC_ACL1
+#define MS_ACL2 KC_ACL2
+
 // How long it takes for a tap to become a hold. Primarily added because
 // Shift-combinations were too quick and causing a parentheses to be added.
 #ifdef TAPPING_TERM
