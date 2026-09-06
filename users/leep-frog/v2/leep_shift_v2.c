2dfaf5d9f3253f3b5874a7e3a9f3711d26732af0
leep-frog <gleeper49@gmail.com>
2026-09-05 20:22:21 -0400

    ctrl-k updates

--- users/leep-frog/v2/leep_shift_v2.c
+++ users/leep-frog/v2/leep_shift_v2.c
@@ -53,8 +53,19 @@ bool kill_line_hold = false;
 
 // Runs emacs line kill (ctrl-k) if hit once, otherwise emulates behavior for regular text things.
 void TDKillLine_finished(tap_dance_state_t *state, void *user_data) {
+    // While in shift mode, both a tap and a hold should just run the full kill-and-yank sequence.
+    if (shift_toggled) {
+        UnsetShift();
+        SEND_STRING(SS_RCTL("k") SS_DELAY(100) SS_PASTE SS_DELAY(50) SS_TAP(X_ENTER));
+        return;
+    }
+
     switch (cur_dance(state, true)) {
         case SINGLE_HOLD:
+            // Only press ctrl+k here. Releasing will result in the paste/enter. That way, flow is:
+            // * Start holding ctrl+k (ctrl+k is pressed)
+            // * Wait for dialog to pop up (ctrl+k is still pressed)
+            // * Release ctrl+k (ctrl+k is released, and paste+enter is sent)
             kill_line_hold = true;
             SEND_STRING(SS_RCTL("k"));
             break;
@@ -77,12 +88,13 @@ void TDKillLine_finished(tap_dance_state_t *state, void *user_data) {
     }
 }
 
+// See SINGLE_HOLD case above as for why this is necessary.
 void TDKillLine_reset(tap_dance_state_t *state, void *user_data) {
     // Using cur_dance doesn't work here (maybe the code thinks there's an interrupt when not?),
     // hence why we maintain and use our own kill_line_hold variable here instead.
     if (kill_line_hold) {
         kill_line_hold = false;
-        SEND_STRING(SS_PASTE);
+        SEND_STRING(SS_PASTE SS_DELAY(50) SS_TAP(X_ENTER));
     }
 }
 
